ff5e97e3768c92136724a5408a63342cda3dd799
justinj26 <63942113+justinj26@users.noreply.github.com>
2021-04-17 22:30:48 -0400

    little bit of sign up page

--- src/SignUp.js
+++ src/SignUp.js
@@ -1,8 +1,17 @@
 import React from "react";
 
-
 class SignUp extends React.Component {
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      newUser: {}
+    };
+  }
+
+  render() {
+    return <div></div>;
+  }
 }
 
 export default SignUp;
